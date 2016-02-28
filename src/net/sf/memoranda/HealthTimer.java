0a678684e98b869231417cb955b89698eac5b5af
tcatherm <tcatherm@asu.edu>
2016-02-28 09:39:39 -0500

    Finished health timer thread and implemented health tab in prefrences

--- src/net/sf/memoranda/HealthTimer.java
+++ src/net/sf/memoranda/HealthTimer.java
@@ -8,27 +8,30 @@ public class HealthTimer extends Thread {
 
 	public HealthTimer(int minutes) {
 		this.waitTime = minutes * 60 * 1000;
+		frame  = new JFrame();
+		frame.setAlwaysOnTop(true);
 	}
 
 	@Override
 	public void run() {
 		try {
-			Thread.sleep(1);
-			createPopUp();
-			// show pop up panel with reminder
-			// wait for 10 seconds or until 'OK' is clicked
-
+			System.out.println("IT'S RUNNING **************************");
+			isRunning = true;
+			while (true) {
+				Thread.sleep(waitTime);
+				createPopUp();
+			}
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	
-	private void createPopUp()
-	{
-		JFrame frame = new JFrame();
+
+	private void createPopUp() {
 		JOptionPane.showMessageDialog(frame, "Time to take your eyes of the screen and stand up for 2 minutes.");
 	}
-
+	
+	private JFrame frame;
 	private long waitTime;
+	private boolean isRunning = false;
 }
